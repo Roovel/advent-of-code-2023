181b02ba4413c5a3347170b79d78aecc45413e1f
Ragnar Öövel <oovel@rushstreetinteractive.com>
2023-12-04 09:51:21 +0200

    day 4 initial commit

--- src/main/java/day03/Processor.java
+++ src/main/java/day03/Processor.java
@@ -9,12 +9,13 @@ import util.TextUtil;
 public class Processor {
 
     private ArrayList<String> engineSchematic = new ArrayList<>();
-    //    private final TextUtil textUtil = new TextUtil("src/Main/resources/inputs/test");
-    private final TextUtil textUtil = new TextUtil("src/main/resources/inputs/03/input03.txt");
+    private final TextUtil textUtil = new TextUtil("src/Main/resources/inputs/test");
+    //    private final TextUtil textUtil = new TextUtil("src/main/resources/inputs/03/input03.txt");
 
     public void getPart1Result() {
         getEngineSchematic();
-        checkNumberPerimeter(engineSchematic);
+        //        part1Solution(engineSchematic);
+        part2Solution(engineSchematic);
         /*System.out.println("-----");
         System.out.println("PART 1");
         System.out.println("The sum of all of the part numbers in the engine schematic is: " + sumOfPartNumbers());
@@ -32,7 +33,84 @@ public class Processor {
         engineSchematic = textUtil.linesToArrayList();
     }
 
-    private void checkNumberPerimeter(ArrayList<String> schematic) {
+    private void part2Solution(ArrayList<String> schematic) {
+        String regex = "\\*";
+        Pattern pattern = Pattern.compile(regex);
+        int sum = 0;
+
+        for (int i = 0; i < schematic.size(); i++) {
+            Matcher matcher = pattern.matcher(schematic.get(i));
+            while (matcher.find()) {
+                String number = matcher.group();
+                int endIndex = matcher.end() - 1;
+                int rightIndex = matcher.end();
+                int leftIndex = matcher.start() - 1;
+                int diagonalStartIndex = matcher.start() - 1;
+                int diagonalEndIndex = matcher.end() + 1;
+
+                if (schematic.get(i).length() + 1 <= diagonalEndIndex) {
+                    diagonalEndIndex = matcher.end();
+                } else if (schematic.get(i).length() - 1 <= diagonalEndIndex) {
+                    diagonalEndIndex = matcher.end() + 1;
+                }
+
+                if (diagonalStartIndex < 0) {
+                    diagonalStartIndex = 0;
+                }
+
+                if (schematic.get(i).length() - 1 <= endIndex) {
+                    rightIndex = endIndex;
+                }
+
+                if (leftIndex < 0) {
+                    leftIndex = 0;
+                }
+
+                if (i == 0) {
+                    System.out.println("First line");
+                    System.out.println("Below: " + schematic.get(i + 1).substring(diagonalStartIndex, diagonalEndIndex));
+                    if (schematic.get(i + 1).substring(diagonalStartIndex, diagonalEndIndex).matches("[\\d.]*") &&
+                            schematic.get(i).substring(leftIndex, leftIndex + 1).matches("[\\d.]*") &&
+                            schematic.get(i).substring(rightIndex, rightIndex + 1).matches("[\\d.]*")) {
+                        System.out.println("no *");
+                    } else {
+                        System.out.println("yes *");
+
+                    }
+                } else if (i + 1 == schematic.size()) {
+                    System.out.println("Above: " + schematic.get(i - 1).substring(diagonalStartIndex, diagonalEndIndex));
+                    if (schematic.get(i - 1).substring(diagonalStartIndex, diagonalEndIndex).matches("[\\d.]*") &&
+                            schematic.get(i).substring(leftIndex, leftIndex + 1).matches("[\\d.]*") &&
+                            schematic.get(i).substring(rightIndex, rightIndex + 1).matches("[\\d.]*")) {
+                        System.out.println("no *");
+                    } else {
+                        System.out.println("yes *");
+
+                    }
+                    System.out.println("Last line");
+                } else {
+                    System.out.println("Above: " + schematic.get(i - 1).substring(diagonalStartIndex, diagonalEndIndex));
+                    System.out.println("Below: " + schematic.get(i + 1).substring(diagonalStartIndex, diagonalEndIndex));
+                    if (schematic.get(i - 1).substring(diagonalStartIndex, diagonalEndIndex).matches("[\\d.]*") &&
+                            schematic.get(i + 1).substring(diagonalStartIndex, diagonalEndIndex).matches("[\\d.]*") &&
+                            schematic.get(i).substring(leftIndex, leftIndex + 1).matches("[\\d.]*") &&
+                            schematic.get(i).substring(rightIndex, rightIndex + 1).matches("[\\d.]*")) {
+                        System.out.println("is not part");
+                    } else {
+                        System.out.println("is part");
+
+                    }
+                }
+
+                System.out.println("Number: " + number);
+                System.out.println("Left: " + schematic.get(i).charAt(leftIndex));
+                System.out.println("Right: " + schematic.get(i).charAt(rightIndex));
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private void part1Solution(ArrayList<String> schematic) {
         String regex = "\\b\\d{1,3}\\b";
         Pattern pattern = Pattern.compile(regex);
         int sum = 0;
